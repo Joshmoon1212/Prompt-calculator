cffb9e09897dc5a21ce5eef117b4e9e3a1252aa6
NO NAME <95919305+Joshmoon1212@users.noreply.github.com>
2024-10-11 17:17:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let operator = prompt("enter an operator : (+, -, /, *)");
+let num1 = parseFloat(prompt("enter the first number: "));
+let num2 = parseFloat(prompt("enter the second number: "));
+
+if (operator ==="+") {
+result = num1 + num2;
+} else if (operator === "-"){
+    result = num1 - num2;
+} else if (operator === "*"){
+    result = num1 * num2
+} else if (operator === "/"){
+    result = num1 / num2
+} else {
+    result = "invalid Operator";
+}
+
+alert("the result is " +  result);
